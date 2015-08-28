07bf403afcf3f6aebb706bcbaf815be851e91ff7
David Bailey <davidbailey@users.noreply.github.com>
2015-08-28 08:53:09 -0700

    Update TransportSim.scala

--- TransportSim.scala
+++ TransportSim.scala
@@ -9,7 +9,7 @@ object Person {
     Bus = 100, speed, 90
     Car = 90, speed, 90
 }
-
+#http://www.scala-lang.org/api/current/index.html#scala.collection.mutable.LazyBuilder
 val P1 = Person
 val P2 = Person
 
