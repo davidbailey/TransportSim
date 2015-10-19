971ea96c21befab59701b4d6246750b8c95d4825
David Bailey <dabailey@davidabailey.com>
2015-10-18 18:09:12 -0700

    updated peopleModel

--- todo/peopleModel.py
+++ todo/peopleModel.py
@@ -23,20 +23,26 @@ for tracts in tractList:
     if tract['geometry'].within(boundingBox):
       tractsInBox.append(tract)
 
-# ca_xwalk = pandas.DataFrame.from_csv(os.path.expanduser('~/Desktop/maps/ca_od_2013/ca_xwalk.csv'), index_col=False)
+tracts = geopandas.GeoDataFrame(tractsInBox)
 
 ca_od_main_2013s = []
 for i in range(1,6):
-  ca_od_main_i_2013 = pandas.DataFrame.from_csv(os.path.expanduser('~/Desktop/maps/ca_od_2013/ca_od_main_JT0' + i + '_2013.csv'), index_col=False)
+  ca_od_main_i_2013 = pandas.DataFrame.from_csv(os.path.expanduser('~/Desktop/maps/ca_od_2013/ca_od_main_JT0' + str(i) + '_2013.csv'), index_col=False)
   ca_od_main_2013s.append(ca_od_main_i_2013)
-  
+
 ca_od_main_2013 = pandas.concat(ca_od_main_2013s)
-# w_coded = pandas.merge(ca_od_main_2013, ca_xwalk, how='inner', left_on='w_geocode', right_on='tabblk2010')
-# wh_coded = pandas.merge(w_coded, ca_xwalk, how='inner', left_on='h_geocode', right_on='tabblk2010')
+
+first11 = lambda x: '0' + unicode(x)[0:10]
+
+ca_od_main_2013['wGEOID'] = ca_od_main_2013['w_geocode']
+ca_od_main_2013['wGEOID'] = ca_od_main_2013['wGEOID'].apply(first11)
+ca_od_main_2013['hGEOID'] = ca_od_main_2013['h_geocode']
+ca_od_main_2013['hGEOID'] = ca_od_main_2013['hGEOID'].apply(first11)
+
+w_coded = pandas.merge(ca_od_main_2013, tracts, how='inner', left_on='wGEOID', right_on='GEOID')
+wh_coded = pandas.merge(w_coded, tracts, how='inner', left_on='hGEOID', right_on='GEOID', suffixes=['W','H'])
 
 trips = []
 for name, row in wh_coded.iterrows():
   for i in range(0,row['S000'])
-    trips.append((oGeometry,dGeometry))
-  
-# connect to osm and route ^ (this is route.py|scala)
+    trips.append((Hgeometry,Wgeometry))
