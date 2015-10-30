75d896a701ff677e3bab8e751e7b2690b403a833
David Bailey <dabailey@davidabailey.com>
2015-10-30 11:28:09 -0700

    modified:   build.scala

--- project/build.scala
+++ project/build.scala
@@ -29,7 +29,7 @@ object TransportsimBuild extends Build {
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
 //        "io.plasmap" %% "geow" % "0.3.6-SNAPSHOT"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
