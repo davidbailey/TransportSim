b7534a1f7bfd5d950a35a0c7076fe94fdb1d3777
David Bailey <davidbailey@users.noreply.github.com>
2015-12-02 01:20:02 -0800

    Update build.scala

--- project/build.scala
+++ project/build.scala
@@ -29,9 +29,9 @@ object TransportSimBuild extends Build {
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
        // "org.apache.spark" %% "spark-core" % "1.5.1"
-//        "io.plasmap" %% "geow" % "0.3.6-SNAPSHOT"
+        "io.plasmap" %% "geow" % "0.3.6-SNAPSHOT"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
