341eac5cc32bb31d5218af807f10c692850e7e75
David Bailey <davidbailey@users.noreply.github.com>
2015-12-07 23:17:04 -0800

    Update build.scala

--- project/build.scala
+++ project/build.scala
@@ -31,8 +31,8 @@ object TransportSimBuild extends Build {
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
         "io.plasmap" %% "geow" % "0.3.11-SNAPSHOT",
-        "org.apache.kafka" %% "kafka" %"0.9.0.0"//,
-        //"org.apache.spark" %% "spark-core" % "1.5.1"
+        "org.apache.kafka" %% "kafka" %"0.9.0.0"
+        "org.apache.spark" %% "spark-core" % "1.5.1
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
