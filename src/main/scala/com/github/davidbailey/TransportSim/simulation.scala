a2f6741b7c9359a9ba58f8cf48e070ef8e0f69aa
David Bailey <davidbailey@users.noreply.github.com>
2015-12-07 22:06:58 -0800

    Update simulation.scala

--- src/main/scala/com/github/davidbailey/TransportSim/simulation.scala
+++ src/main/scala/com/github/davidbailey/TransportSim/simulation.scala
@@ -5,7 +5,7 @@ import Polyline.decode //:load Polyline.scala
 import io.plasmap.parser.OsmParser // https://github.com/plasmap/geow
 //import org.apache.spark.{SparkContext, SparkConf}
 import java.util.Properties
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord} // http://kafka.apache.org/090/javadoc/org/apache/kafka/clients/producer/KafkaProducer.html
 
 
 object Main {
@@ -47,15 +47,18 @@ object Main {
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
   val producer = new KafkaProducer[AnyRef, AnyRef](props)
-  producer.send(new ProducerRecord("topic","testkey","testvalue"))
-  producer.close()
   
   for (a <- 1 to 10) {
     println("\n\nRound " + a + "\n")
-    People.map(p => p.view)
     People.map(p => p.transport)
-    Cars.map(c => c.view)
-    Bicycles.map(b => b.view)
+    val peopleView = People.map(p => p.view)
+    val carView = Cars.map(c => c.view)
+    val bicycleView = Bicycles.map(b => b.view)
+    producer.send(new ProducerRecord("people", a.toString, peopleView))
+    producer.send(new ProducerRecord("cars", a.toString, carView))
+    producer.send(new ProducerRecord("bicycles", a.toString, bicycleView))
     //Thread.sleep(1000)
   }
+  producer.close()
+
 }
